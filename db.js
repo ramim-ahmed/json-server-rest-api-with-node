daf3c6d328dd8930be3fa64bacc80c31c802d06f
ramimcodes <ramimahmedraj2@gmail.com>
2022-12-07 04:03:38 +0600

    build-fake-json-server

---
+++ db.js
@@ -0,0 +1,12 @@
+const todos = require('./data/todos.json');
+const videos = require('./data/videos.json');
+const tags = require('./data/tags.json');
+const transactions = require('./data/transactions.json');
+const products = require('./data/products.json');
+module.exports = {
+    todos,
+    videos,
+    tags,
+    transactions,
+    products
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const data = require('./db.js');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+// Bind the router db to the app
+server.db = router.db;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
